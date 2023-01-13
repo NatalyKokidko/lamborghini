161f691c21e3857a0a087deb1d9563887f10be0d
NatalyKokidko <nkokidko25@gmail.com>
2023-01-13 13:41:57 +0100

    add css adaptive, midifiet html, and add js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click', ()=> {
+    menuMobile.classList.toggle('menu--open');
+});
+
 const swiper = new Swiper('.swiper', {
     loop: true,
    slidesPerView: 2,
@@ -6,6 +13,16 @@ const swiper = new Swiper('.swiper', {
      nextEl: '.swiper-button-right',
      prevEl: '.swiper-button-left',
    },
+   breakpoints: {
+    0: {
+        slidesPerView: 1,
+   },
+   768: {
+    slidesPerView: 2,
+    spaceBetween: 20,
+   }
+}
+
  });
 
  let map;
